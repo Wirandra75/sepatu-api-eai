c77d785dc2960862be3d3b8f211ecc4b8d7e2453
Rafie <rafiewirandra75@gmail.com>
2023-04-27 12:41:31 +0700

    push ulang

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mysql2 = require("mysql2");
 const db = mysql2.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
 });
 
